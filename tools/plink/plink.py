58c22b119d3d8f53eeda9fb1fd970a7756509d6f
mike <msolo@users.noreply.github.com>
2012-12-21 11:33:42 -0800

    allow pure data files in achive (compatible with pkgutil) handle pyc files correct when linktime and runtime python versions differ correct strack trace information in the bootstrap and run modules default to explicit python version in shebang

--- tools/plink/plink.py
+++ tools/plink/plink.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/local/bin/python2.7
 
 # Link a python source tree into a single self-executing zip archive.
 # The zip can also be extracted and run manually.
@@ -16,7 +16,7 @@ import string
 import sys
 import zipfile
 
-bootstrap_src_tmpl = """
+bootstrap_src_tmpl = r"""
 # ParImporter bootstrap
 # 
 # Insert a custom importer to allow compressed .so files to load transparently.
@@ -27,21 +27,32 @@ import cStringIO
 import errno
 import hashlib
 import imp
+import marshal
 import os
 import sys
 import tempfile
+import traceback
 import zipfile
 import zipimport
 
+compile_pymagic = ${compile_pymagic}
+
 newname = '_par_bootstrap_'
-sys.modules[newname] = sys.modules[__name__]
+_par_bootstrap_ = sys.modules[__name__]
+_par_bootstrap_.__name__ = newname
+sys.modules[newname] = _par_bootstrap_
 del sys.modules[__name__]
 
 def log(fmt, *args):
   if os.environ.get('PLINK_DEBUG'):
     if args:
       fmt = fmt % args        
-    print >> sys.stderr, 'plink:', fmt
+    print >> sys.stderr, 'plink: debug', fmt
+
+def log_warning(fmt, *args):
+  if args:
+    fmt = fmt % args        
+  print >> sys.stderr, 'plink: warning', fmt
 
 class ParImporter(zipimport.zipimporter):
   def __init__(self, path):
@@ -69,7 +80,7 @@ class ParImporter(zipimport.zipimporter):
       pass
     
     return zipimport.zipimporter.find_module(self, fullname, path)
-  
+
   def load_module(self, fullname):
     if fullname in sys.modules:
       log('load_module %s (cached)', fullname)
@@ -107,7 +118,8 @@ class ParImporter(zipimport.zipimporter):
       raise ImportError('No module for %s (plink)' % fullname)
 
     log('load_module %s (default)', fullname)
-    return zipimport.zipimporter.load_module(self, fullname)
+    module = zipimport.zipimporter.load_module(self, fullname)
+    return module
 
 log('initial sys.path: %s', sys.path)
 sys.path_hooks.insert(0, ParImporter)
@@ -116,29 +128,71 @@ m = hashlib.md5()
 m.update(open(sys.path[0]).read())
 par_signature = m.hexdigest()
 
+# Override the source returned. Otherwise the __run__ and __main__
+# modules get confused because both must be run as main to preserve
+# existing script behaviors.
+class SourceLoader(object):
+  def __init__(self, loader, source_map):
+    self.loader = loader
+    self.source_map = source_map
+
+  def __getattr__(self, name):
+    return getattr(self.loader, name)
+  
+  def get_source(self, fullname):
+    if fullname in self.source_map:
+      return self.source_map[fullname]
+    return self.loader.get_source(fullname)
+
 zf = zipfile.ZipFile(sys.path[0], 'r')
-zf_data = zf.read('__run__.py')
+source_map = {
+  '_par_bootstrap_': zf.read('__main__.py'),
+  '__main__': zf.read('__run__.py'),
+  }
+
+__loader__ = SourceLoader(__loader__, source_map)
+
+run_globals = {
+  '__name__': '__main__',
+  '__loader__': __loader__,
+}
+
+runtime_pymagic = imp.get_magic() + '\000\000\000\000'
+if runtime_pymagic != compile_pymagic:
+  log_warning('runtime / linktime mismatch - ignoring .pyc files')
+  run_py = zf.read('__run__.py')
+  run_code = compile(run_py, '__run__.py', 'exec')
+else:
+  run_pyc = zf.read('__run__.pyc')
+  run_code = marshal.loads(run_pyc[len(runtime_pymagic):])
 zf.close()
-exec zf_data
+
+try:
+  exec run_code in run_globals
+except:
+  # Force the pure-python traceback printer, which correctly calls the loader
+  # to resolve traceback sources.
+  traceback.print_exc()
 """
 
 
 tracer = 'plink-tracer-19830319'
 zipmagic = '\x50\x4B\x03\x04'
-pymagic = imp.get_magic() + '\000\000\000\000'
+compile_pymagic = imp.get_magic() + '\000\000\000\000'
 
 def mkbootstrap():
-  return string.Template(bootstrap_src_tmpl).substitute(tracer=tracer)
+  tmpl = string.Template(bootstrap_src_tmpl)
+  return tmpl.substitute(tracer=tracer, compile_pymagic=repr(compile_pymagic))
 
 # Takes a directory and makes a zip file containing any files therein
 # that are (normally) loadable by Python.
-def zipdir(path, options):
-  compileall.compile_dir(path, ddir='', quiet=True)
-  path = os.path.normpath(path)
-  prefix = path + '/'
+def zipdir(source_root, data_paths, options):
+  source_root = os.path.normpath(source_root)
+  compileall.compile_dir(source_root, ddir='', quiet=True)
+  prefix = source_root + '/'
   b = cStringIO.StringIO()
   z = zipfile.ZipFile(b, 'w', zipfile.ZIP_DEFLATED)
-  for root, dirs, files in os.walk(path):
+  for root, dirs, files in os.walk(source_root):
     for f in sorted(files):
       if f.endswith(('.py', '.pyc', '.pyo', '.so')):
         if options.strip and f.endswith('.py'):
@@ -147,6 +201,12 @@ def zipdir(path, options):
         arcpath = realpath.replace(prefix, '')
         log('deflate %s -> %s', realpath, arcpath)
         z.write(realpath, arcpath)
+  for data_path in sorted(data_paths):
+    realpath = os.path.normpath(data_path)
+    arcpath = realpath.replace(prefix, '')
+    log('deflate %s -> %s', realpath, arcpath)
+    z.write(realpath, arcpath)
+    
   z.close()
   return b.getvalue()
 
@@ -158,7 +218,7 @@ def log(fmt, *args):
 
 options = None
 usage = """
-%prog --main-file <main source file> <src directory>
+%prog --main-file <main source file> <zip source directory> [<data file>, ...]
 
 PLINK_DEBUG=1 ./module.par
 """
@@ -171,7 +231,7 @@ if __name__ == '__main__':
   p.add_option('--format', default='par')
   p.add_option('--main-file',
                help='name of python to run after bootstrap')
-  p.add_option('--python-binary', default='/usr/bin/python -ESs',
+  p.add_option('--python-binary', default='/usr/local/bin/python2.7 -ESs',
                help='adjust the shebang line of the output')
   (options, args) = p.parse_args()
 
@@ -182,28 +242,28 @@ if __name__ == '__main__':
   if not options.main_file:
     sys.exit('--main-file must be specified')
 
-  zipfn = args.pop(0)
+  zip_root = args.pop(0)
   if not options.output:
     options.output = '%s.%s' % (os.path.basename(options.main_file).rsplit('.', 1)[0], options.format)
 
-  if not os.path.isdir(zipfn):
+  if not os.path.isdir(zip_root):
     sys.exit('source must be a directory')
     
   bootstrap_src = mkbootstrap()
-  log(bootstrap_src)
+  log('\n'.join(['%03d  %s' % (i+1, line) for (i, line) in enumerate( bootstrap_src.split('\n'))]))
 
   with open(options.main_file) as f:
     main_src = f.read()        
-  with open(os.path.join(zipfn, '__main__.py'), 'w') as f:
+  with open(os.path.join(zip_root, '__main__.py'), 'w') as f:
     f.write(bootstrap_src)
-  with open(os.path.join(zipfn, '__run__.py'), 'w') as f:
+  with open(os.path.join(zip_root, '__run__.py'), 'w') as f:
     f.write(main_src)
 
-  zipdata = zipdir(zipfn, options)
+  zipdata = zipdir(zip_root, args, options)
 
   if options.format == 'pyc':
     bootstrap_code = compile(bootstrap_src, '<plink bootstrap>', 'exec')
-    code = pymagic + marshal.dumps(bootstrap_code) + zipdata
+    code = compile_pymagic + marshal.dumps(bootstrap_code) + zipdata
   elif options.format == 'par':
     code = '#!%s\n' % options.python_binary + zipdata
 
